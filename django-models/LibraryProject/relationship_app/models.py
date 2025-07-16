86abcf2b58acf016bc25ba4f4cee1edd6e1c5343
Yeabsira Taye <yeabsira.taye@aastustudent.edu.et>
2025-07-16 12:05:17 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -31,9 +31,17 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # Changed from CharField to ForeignKey
     published_date = models.DateField()
 
     def __str__(self):
