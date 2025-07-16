ee77f57d871a66cc7141476c97ba7395cd6af7a1
Yeabsira Taye <yeabsira.taye@aastustudent.edu.et>
2025-07-16 10:54:23 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path('member-view/', views.member_view, name='member_view'),
 
     path('books/', views.list_books, name='list_books'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
